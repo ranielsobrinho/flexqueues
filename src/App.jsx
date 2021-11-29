751be35028f979b01f1fd314a107f03715d0bbf8
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-29 16:33:15 -0300

    refactor: init routing with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './style.css'
 import Home from './pages/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/ >
+      </Routes>
+    </Router>
   );
 }
 
