d83fa0f8e7a7ac803b3ea9f8d353f16891833cd7
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-29 10:27:21 -0300

    feat: add home page component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+import { Button } from 'antd'
+
+export default function Home() {
+    return(
+        <div>
+            <h2>Hello World from Home component</h2>
+
+            <Button type="primary">Let's go</Button>
+        </div>
+    )
+}
