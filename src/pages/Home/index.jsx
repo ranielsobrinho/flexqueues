a7ba2c84b45144c429ed3eef4a7b5ff6d9478be3
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-29 15:08:36 -0300

    refactor: rendering sign in component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import './index.css'
-
-import { Button } from 'antd'
+import SignForm from '../../components/SignForm'
 
 export default function Home() {
     return(
         <div>
-            <h2>Hello World from Home component</h2>
-
-            <Button type="primary">Let's go</Button>
+            <SignForm />
         </div>
     )
 }
