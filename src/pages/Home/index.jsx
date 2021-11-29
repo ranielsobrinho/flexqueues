ec7b7e36f9a9191e7aa6ba910ea93e562340bade
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-29 16:35:59 -0300

    feat: using state of react to handle forms

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
+import { Button } from 'antd'
 import SignForm from '../../components/SignForm'
+import RegisterForm from '../../components/RegisterForm'
 
 export default function Home() {
+    const [ login, setLogin ] = useState(true)
+
+    function toggleLogin(){
+        setLogin(!login)
+    }
     return(
-        <div>
-            <SignForm />
+        <div className='container'>
+            { login ? <SignForm /> : <RegisterForm /> }
+
+            <p>Não tem conta?</p>
+            <Button type='link' onClick={() => {toggleLogin()}}>Registre-se aqui</Button>
         </div>
-    )
+    ) 
 }
