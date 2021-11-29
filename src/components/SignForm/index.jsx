24fcb9dce94d0120cddea61f4ba0bdfc2a727dd6
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-29 16:34:39 -0300

    feat: add navigation to redirect user to main page

--- src/components/SignForm/index.jsx
+++ src/components/SignForm/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 import { Button } from 'antd'
 import Api from '../../service/Api'
 
 export default function SignForm() {
     const {register, handleSubmit} = useForm()
+    const navigate = useNavigate()
 
     const submit = data => {
         console.log(data);
         Api.post('/api/auth', data)
         .then((res) => {
             sessionStorage.setItem('token', res.data.data)
+            navigate("/queues")
         }).catch((err) => console.log(err))
     }
     return (
