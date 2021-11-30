b946eaa9b37dba3cc1b45d631eeaff7942304306
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-11-30 08:15:05 -0300

    feat: rendering queue page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './style.css'
-import Home from './pages/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Queues from './pages/Queues'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}/ >
+        <Route path="/queues" element={<Queues />}/ >
       </Routes>
     </Router>
   );
