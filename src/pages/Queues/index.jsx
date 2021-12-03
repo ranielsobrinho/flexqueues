15e3b3d0ac0f5f05bc8c1a5c83e23fdefe58ed70
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-03 16:49:19 -0300

    feat: request data from api and displaying it

--- src/pages/Queues/index.jsx
+++ src/pages/Queues/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Api from '../../service/Api'
 
 export default function Queues() {
-    return (
-        <div>
-            <h1>Hello from queues page</h1>
-        </div>
-    )
+	const [ queues, setQueues ] = useState([])
+
+	useEffect(() => {
+		Api.get('/api/queues')
+			.then(({data}) => {
+				setQueues(data)
+				console.log(data)
+			})
+			.catch(err => console.log(err))
+	}, [])
+
+	function testQueueContent () {
+		alert(queues)
+	}
+
+	return (
+		<div className="containerQueue">
+			{queues ? queues
+				.map((queue, id) => {
+					return (
+						<div key={id}>
+							<p>{queue.name}</p>
+						</div>
+					)
+				})
+							:
+							<div>
+								<p>Hello world</p>
+								<button onClick={() => testQueueContent()}>Click here</button>
+							</div>
+					}
+			</div>
+	)
 }
