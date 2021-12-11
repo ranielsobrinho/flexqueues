59d761bd06ac958fcbfead20d882fa6b00bda63e
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-11 16:22:07 -0300

    feat: add modal to add a queue

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Layout,Button } from 'antd'
-import { RocketOutlined, LogoutOutlined } from '@ant-design/icons'
+import { Layout,Button, Modal, message } from 'antd'
+import { RocketOutlined, LogoutOutlined, PlusCircleOutlined } from '@ant-design/icons'
 import { useNavigate } from 'react-router-dom'
 import './index.css'
+import QueueForm from '../QueueForm'
 
 
 export default function HeaderComponent() {
@@ -13,11 +14,28 @@ export default function HeaderComponent() {
     sessionStorage.removeItem('token')
     navigate('/')
   }
+
+  function info(){
+    Modal.info({
+      title: 'Nova fila, clique em Ok para retornar',
+      content: (
+        <QueueForm />
+      ),
+      onOk(){
+        message.info('Modal fechado.')
+      },
+      width: 700
+    })
+  }
+
   return (
     <div>
       <Header>
         <p className="headerTitle">FlexQueues<RocketOutlined /></p>
-        <Button type='link' onClick={() => logOut()}>Sair<LogoutOutlined /></Button>
+        <div>
+          <Button type='primary' onClick={info}><PlusCircleOutlined />Nova fila</Button>
+          <Button type='link' onClick={() => logOut()}>Sair<LogoutOutlined /></Button>
+        </div>
       </Header>
     </div>
   )
